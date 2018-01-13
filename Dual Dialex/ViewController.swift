f4cb9be53ef44aceb445c5688305ad764906b779
Hunter Nuss <HunterNuss@harrys-imac.fios-router.home>
2018-01-13 18:38:03 -0500

    Initial Commit

---
+++ Dual Dialex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dual Dialex
+//
+//  Created by Hunter Nuss on 1/13/18.
+//  Copyright © 2018 Hunter Nuss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
