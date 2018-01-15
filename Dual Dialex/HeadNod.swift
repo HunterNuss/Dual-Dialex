ff541e6273497961a5cb3c21935efbf419eabd1a
Hunter Nuss <HunterNuss@harrys-imac.fios-router.home>
2018-01-14 21:31:48 -0500

    Created Head Nod ViewController

---
+++ Dual Dialex/HeadNod.swift
@@ -0,0 +1,35 @@
+//
+//  HeadNod.swift
+//  Dual Dialex
+//
+//  Created by Hunter Nuss on 1/14/18.
+//  Copyright © 2018 Hunter Nuss. All rights reserved.
+//
+
+import UIKit
+
+class HeadNod: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Dual Dialex/TheIntrinsicHeadNod.swift
@@ -0,0 +1,21 @@
+//
+//  TheIntrinsicHeadNod.swift
+//  Dual Dialex
+//
+//  Created by Hunter Nuss on 1/14/18.
+//  Copyright © 2018 Hunter Nuss. All rights reserved.
+//
+
+import UIKit
+
+class TheIntrinsicHeadNod: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+
+}
